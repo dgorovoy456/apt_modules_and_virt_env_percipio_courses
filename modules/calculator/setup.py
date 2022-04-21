d0e3a914bbc265a26d067d140accb7ddcb5a7811
denys.horovyi <denys.horovyi@globallogic.com>
2022-04-21 11:37:26 +0200

    Lesson 1-3
    - modules/calculator/calc.py and setup.py have been created
    - calc.py contains simple math functions
    - setup.py contains general information about calc package to generate distribution package
    - distribution packages calc.egg-info and dist have been generated

---
+++ modules/calculator/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='calc',
+    version='1.0',
+    description='My new Python module',
+    author='Denys',
+    author_email='dgorovoy456@gmail.com',
+    url='some.come',
+    py_modules=['calc']
+
+)
+
+# python3 setup.py sdist // to convert calc package to distribution file
